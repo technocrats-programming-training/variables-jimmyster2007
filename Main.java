410d9731482f6e6fcd83b9ed8a628c3daf54a1ba
jimmyster2007 <93486196+jimmyster2007@users.noreply.github.com>
2022-06-21 17:41:05 -0500

    fixed location of code

--- Main.java
+++ Main.java
@@ -8,22 +8,19 @@ public class Main {
             int age = Integer.parseInt(args[2]);
         } catch (NumberFormatException ex) {
             ex.printStackTrace();
-            System.out.println("Hello, I am " + firstName + lastName + "and I am " + age + "years old");
-                system.out.println( age % 2 == 0);
-                    boolean sameName = firstName = lastName;
-                    System.out.println(sameName);
-                        boolean amIOld = age > 0;
-                        System.out.println(amIOld);
+     
         }
         
 
         // Print out the following string: "Hello, I am <firstName> <lastName>, and I am <age> years old"
-
+               System.out.println("Hello, I am " + firstName + lastName + "and I am " + age + "years old");
         // Print out whether age is even
-
+                system.out.println( age % 2 == 0);
         // Print out whether firstName equals lastName
-
+                    boolean sameName = firstName = lastName;
+                    System.out.println(sameName);
         // Print out whether age is greater than 0
-
+                    boolean amIOld = age > 0;
+                    System.out.println(amIOld);
     }
 }
